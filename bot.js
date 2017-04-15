c03e39ff46a35df1dac54db1c88faaf8c6123b83
audiosprite <jpfiorilla@gmail.com>
2017-04-15 06:23:19 -0400

    builds tweet

--- bot.js
+++ bot.js
@@ -6,13 +6,32 @@ require.extensions['.txt'] = function (module, filename) {
 };
 const dictionary = require('./dictionary.txt').split("\n");
 
+function randomIndex(max, excepts){
+    return Math.floor(Math.random()*(max+1));
+}
 Array.prototype.pick = function() {
     return this[Math.floor(Math.random()*this.length)];
 }
-const pickAdjective = function(){
+
+const pickAdjective = function(firstLetter, length = 6){
     let word = dictionary.pick();
-    while (rita.getPosTags(word) != 'jj' || word.length > 6) word = dictionary.pick();
-    console.log(word, rita.getPosTags(word));
+    while (!rita.getPosTags(word).includes('jj') || word.length < 3 || word.length > length || (firstLetter && word[0] != firstLetter.toLowerCase())) word = dictionary.pick();
     return word;
 }
-pickAdjective()
+
+const findLines = function(adjective){
+    let lines = [];
+    for (var i = 0; i < adjective.length; i++) lines.push(pickAdjective(adjective[i]));
+    return lines;
+}
+
+const buildAcrostic = function(){
+    let acrostic = [], adjective = pickAdjective().toUpperCase().split(''), lines = findLines(adjective);
+    let dontReplaceFirstCharIndex = randomIndex(adjective.length);
+    for (var i = 0; i < adjective.length; i++){
+        acrostic.push(adjective[i] + (i != dontReplaceFirstCharIndex ? lines[i].slice(1) : lines[i]));
+    }
+    console.log(['why yes im ' + adjective.join(''), acrostic.join('\n')].join('\n'))
+    return acrostic.join('\n')
+}
+buildAcrostic();
